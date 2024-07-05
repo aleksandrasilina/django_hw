9850acb803a1034502bd79c67ed1b06f26a9221d
Александра <aleksandrasilina@gmail.com>
2024-07-05 09:52:06 +0300

    change settings.py for database connection

--- config/settings.py
+++ config/settings.py
@@ -51,8 +51,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_hw',
+        'USER': 'postgres',
+        'PASSWORD': '123456'
     }
 }
 

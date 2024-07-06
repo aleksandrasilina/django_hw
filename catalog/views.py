5b8c556250f22a1c9ac274ed62159babbf55653d
Александра <aleksandrasilina@gmail.com>
2024-07-06 14:31:30 +0300

    add print(last_5_products) to views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
+import json
+from pprint import pprint
+
 from django.shortcuts import render
 
 
 def home(request):
+    with open("catalog.json", "r", encoding="utf-8") as file:
+        products = [product["fields"] for product in json.load(file) if product["model"] == "catalog.product"]
+        last_5_products = sorted(products, key=lambda x: x["created_at"], reverse=True)[:5]
+        pprint(last_5_products)
     return render(request, 'home.html')
 
 

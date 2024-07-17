deb2c5531a92f801b2c13223631f0d47f19d0598
Александра <aleksandrasilina@gmail.com>
2024-07-17 23:30:32 +0300

    change blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,35 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=150, verbose_name="Заголовок", help_text="Введите заголовок статьи")
-    content = models.TextField(verbose_name="Содержимое", help_text="Введите содержимое")
-    preview = models.ImageField(upload_to="blog/previews", verbose_name="Превью",
-                                help_text="Загрузите изображение для превью", null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания",
-                                      help_text="Укажите дату создания")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликовано",
-                                       help_text="Укажите, опубликована статья или нет")
-    views_count = models.IntegerField(default=0, verbose_name="Количество просмотров",
-                                      help_text="Укажите количество просмотров")
-    slug = models.CharField(max_length=150, verbose_name='Slug', null=True, blank=True)
+    title = models.CharField(
+        max_length=150, verbose_name="Заголовок", help_text="Введите заголовок статьи"
+    )
+    content = models.TextField(
+        verbose_name="Содержимое", help_text="Введите содержимое"
+    )
+    preview = models.ImageField(
+        upload_to="blog/previews",
+        verbose_name="Превью",
+        help_text="Загрузите изображение для превью",
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+        help_text="Укажите дату создания",
+    )
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name="Опубликовано",
+        help_text="Укажите, опубликована статья или нет",
+    )
+    views_count = models.IntegerField(
+        default=0,
+        verbose_name="Количество просмотров",
+        help_text="Укажите количество просмотров",
+    )
+    slug = models.CharField(max_length=150, verbose_name="Slug", null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -20,4 +38,4 @@ class Article(models.Model):
     class Meta:
         verbose_name = "Статья"
         verbose_name_plural = "Статьи"
-        ordering = ['-created_at']
+        ordering = ["-created_at"]

410de1a0a734868c818c5336581835847acea41e
Александра <aleksandrasilina@gmail.com>
2024-07-17 14:45:55 +0300

    change urls.py add ContactView ProductCreateView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, contacts, ProductDetailView
+from catalog.views import ProductListView, ContactView, ProductDetailView, ProductCreateView
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('contacts/', contacts, name='contacts'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
     path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
 ]

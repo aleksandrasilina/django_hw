677a011df180137ddf539e79db624b818cedebd8
Александра <aleksandrasilina@gmail.com>
2024-07-17 14:45:45 +0300

    change viewa.py add ContactView ProductCreateView

--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,9 @@
-import json
-from pprint import pprint
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, TemplateView, CreateView
 
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
-from django.views.generic import DetailView, ListView
-
-from catalog.forms import UploadFileForm
+from catalog.forms import ContactUsForm
 from catalog.models import Product
-
-
-def handle_uploaded_file(f):
-    with open(f"media/catalog/photo/{f.name}", "wb+") as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-
-
-def save_product_data(product_data):
-    with open("new_product.json", "w", encoding="utf-8") as file:
-        json.dump(product_data, file, ensure_ascii=False, indent=4)
 
 
 class ProductListView(ListView):
@@ -33,25 +18,43 @@ class ProductDetailView(DetailView):
     model = Product
 
 
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'photo', 'category', 'price')
+    success_url = reverse_lazy('catalog:product_list')
 
-def contacts(request):
-    context = {
+
+# def contacts(request):
+#     context = {
+#         'title': 'Контакты'
+#     }
+#
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         with open('messages.txt', 'a') as file:
+#             file.write(f'{name}({email}): {message}\n')
+#
+#     return render(request, 'contacts.html', context)
+
+class ContactView(TemplateView):
+    template_name = 'catalog/contacts.html'
+    extra_context = {
         'title': 'Контакты'
     }
 
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        with open('messages.txt', 'a') as file:
-            file.write(f'{name}({email}): {message}\n')
+    def post(self, request):
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            form.save()
+            with open('messages.txt', 'a') as file:
+                file.write(f'{name}({email}): {message}\n')
+        else:
+            ContactUsForm()
 
-    # contacts_data = {}
-    # with open('contacts.json', 'r', encoding='utf-8') as file:
-    #     for i, contact in enumerate(json.load(file), 1):
-    #         contacts_data[str(i)] = contact['fields']
-
-    return render(request, 'contacts.html', context)
-
-
+        return render(request, 'catalog/contacts.html', {'form': form})
 

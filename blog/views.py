bacfd4d6558e998a29e454664bc25ec878eb0572
Александра <aleksandrasilina@gmail.com>
2024-07-17 23:30:58 +0300

    change blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from pytils.translit import slugify
 
 from blog.models import Article
 
@@ -12,21 +13,42 @@ class ArticleListView(ListView):
         'title': 'Блог'
     }
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class ArticleDetailView(DetailView):
     model = Article
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class ArticleCreateView(CreateView):
     model = Article
-    fields = ('title', 'content', 'preview',)
+    fields = ('title', 'content', 'preview', 'is_published',)
     success_url = reverse_lazy('blog:articles_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_article = form.save()
+            new_article.slug = slugify(new_article.title)
+            new_article.save()
+        return super().form_valid(form)
 
 
 class ArticleUpdateView(UpdateView):
     model = Article
-    fields = ('title', 'content', 'preview',)
+    fields = ('title', 'content', 'preview', 'is_published',)
     success_url = reverse_lazy('blog:articles_list')
+
+    def get_success_url(self):
+        return reverse('blog:article_detail', args=[self.kwargs.get('pk')])
 
 
 class ArticleDeleteView(DeleteView):

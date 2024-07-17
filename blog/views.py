7b20622fb9f581ba4c6c2d3fb1b3be56a1962bdd
Александра <aleksandrasilina@gmail.com>
2024-07-17 22:54:16 +0300

    add blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
-# Create your views here.
+from blog.models import Article
+
+
+class ArticleListView(ListView):
+    model = Article
+    paginate_by = 3
+    extra_context = {
+        'title': 'Блог'
+    }
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ('title', 'content', 'preview',)
+    success_url = reverse_lazy('blog:articles_list')
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'content', 'preview',)
+    success_url = reverse_lazy('blog:articles_list')
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = reverse_lazy('blog:articles_list')

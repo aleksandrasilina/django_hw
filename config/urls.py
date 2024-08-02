4bf80d67c04da74780167a39b5066e40b84d8bf0
Александра <aleksandrasilina@gmail.com>
2024-08-02 23:54:50 +0300

    change urls.py + debug_toolbar_urls()

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('catalog.urls', namespace='catalog')),
                   path('blog/', include('blog.urls', namespace='blog'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()

e69b0bf7c4143e0041a1c36971187c6083068df3
Александра <aleksandrasilina@gmail.com>
2024-08-03 15:18:16 +0300

    change forms.py add clean_name. clean_description

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,6 +2,18 @@ from django import forms
 
 from catalog.models import Contact, Product
 
+FORBIDDEN_WORDS = [
+    'казино',
+    'криптовалюта',
+    'крипта',
+    'биржа',
+    'дешево',
+    'бесплатно',
+    'обман',
+    'полиция',
+    'радар'
+]
+
 
 class ContactUsForm(forms.ModelForm):
     class Meta:
@@ -13,3 +25,18 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ('created_at', 'updated_at',)
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        for word in FORBIDDEN_WORDS:
+            if word in name:
+                raise forms.ValidationError('Имя продукта содержит запрещенное слово')
+        return name
+
+
+    def clean_description(self):
+        description = self.cleaned_data['description']
+        for word in FORBIDDEN_WORDS:
+            if word in description:
+                raise forms.ValidationError('Описание продукта содержит запрещенное слово')
+        return description

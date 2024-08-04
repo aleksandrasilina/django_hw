a7136e62f15b5374abfa0eadd565a72d5ccc56f4
Александра <aleksandrasilina@gmail.com>
2024-08-04 23:25:20 +0300

    change catalog/forms.py add StyleFormMixin

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import BooleanField
 
 from catalog.models import Contact, Product
 
@@ -15,13 +16,23 @@ FORBIDDEN_WORDS = [
 ]
 
 
-class ContactUsForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ContactUsForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
 
 
-class ProductForm(forms.ModelForm):
+class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Product
         exclude = ('created_at', 'updated_at',)

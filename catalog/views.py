7f05a9bd85957bebf39fb0220c022c745fbc390c
Александра <aleksandrasilina@gmail.com>
2024-08-05 15:13:51 +0300

    change catalog/views.py add Version formset add active product.version

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, TemplateView, CreateView, UpdateView
 
-from catalog.forms import ContactUsForm, ProductForm
-from catalog.models import Product
+from catalog.forms import ContactUsForm, ProductForm, VersionForm
+from catalog.models import Product, Version
 
 
 class ProductListView(ListView):
@@ -13,6 +14,12 @@ class ProductListView(ListView):
         'title': 'Главная'
     }
 
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        for product in context_data.get('object_list'):
+            product.version = product.version_set.filter(is_active=True).first()
+        return context_data
+
 
 class ProductDetailView(DetailView):
     model = Product
@@ -20,6 +27,7 @@ class ProductDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = context['product'].name
+        self.object.version = self.object.version_set.filter(is_active=True).first()
         return context
 
 
@@ -33,6 +41,26 @@ class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        VersionFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = VersionFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = VersionFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        formset = self.get_context_data()['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+        if self.object.version_set.filter(is_active=True).count() > 1:
+            form.add_error(None, 'Активной может быть только одна версия')
+            return self.form_invalid(form)
+        return super().form_valid(form)
 
 
 class ContactView(TemplateView):
